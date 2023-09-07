10f941da2a77cbfd457510593863bbc77e7f52cc
KathyA <100660595+kitkat-creat@users.noreply.github.com>
2023-09-06 20:03:48 -0700

    show resume

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-import Resume from "./components/Resume";
+import Resume from "./components/Resume/SWERESUME.pdf";
 import {
   BrowserRouter as Router,
   Route,
